cbd8dda2ef9b54adbc20b5ddeae00b506464d6c9
Sayantan Chatterjee <thisissayantan.mail@gmail.com>
2018-12-11 04:32:55 +0530

    Add documentation

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # TMDb Search App using Django
+
+A movie-search engine based on Django, powered by TMDb.
+
+## Contents
+- [TMDb Search App using Django](#tmdb-search-app-using-django)
+    - [Contents](#contents)
+    - [Platform](#platform)
+    - [Dependencies](#dependencies)
+    - [Usage](#usage)
+        - [Install Dependencies](#install-dependencies)
+    - [Run](#run)
+    - [Live Demo](#live-demo)
+
+## Platform
+
+```
+    python: 3.7.1
+    vs code: 1.25.1
+    ubuntu: 18.04
+```
+
+## Dependencies
+
+- **`Django`**: A high-level Python web framework.
+- **`tmdbsimple`**: A Python wrapper on the TMDb V3 API.
+- **`gunicorn`**: A Python WSGI HTTP Server for UNIX, used to host the webiste on Heroku.
+- **`whitenoise`**: A simple static file serving package for Python web apps.
+
+## Usage
+
+Dependencies must be set up in an enviroment before using the project. Follow the following instructions for setting up the environment:
+
+### Install Dependencies
+
+- **For pip:**
+  ```
+    python3 -m virtualenv <environment name>
+    source <environment name>/bin/activate
+    pip install -r requirements.txt
+  ```
+- **For Anaconda:**
+  ```
+    conda env create -f environment.yml
+    source activate django-tmdb
+    pip install -r requirements.txt
+  ```
+
+## Run
+    ```
+        python manage.py runserver
+        OR
+        gunicorn pytmdb.wsgi:application --log-file -
+    ```
+
+## [Live Demo](https://django-tmdb.herokuapp.com)
+
+This web app has been deployed live on Heroku.
+
+<img src="https://i.imgur.com/InHkGUW.jpg" alt="">
+<img src="https://i.imgur.com/9U7UM7P.png" alt="">
--- movies/views.py
+++ movies/views.py
@@ -5,11 +5,21 @@ import os
 
 config = ConfigParser()
 config.read('movies/config.cfg')
-print(os.getcwd())
 tmdb.API_KEY = config['tmdb']['API_KEY']
 
 # Create your views here.
 def home(request):
+    """This is a function-based view to serve
+    the movie list for a particular search query,
+    using tmdbsimple API calls.
+
+    Params:
+        request: request from the front-end API call
+
+    Returns:
+        render method at the movie.html endpoint with
+        the required search results.
+    """
     query = str(request.GET.get('query', ''))
     if query != '':
         search_result = tmdb.Search().movie(query=query)['results']
@@ -25,22 +35,33 @@ def home(request):
     return render(request, "movie.html", frontend)
 
 def details(request, id=None):
-    if id:
-        movie = tmdb.Movies(id)
-        trailers = list(filter(lambda v: v['type'] == 'Trailer', movie.videos()['results']))
-        teasers = list(filter(lambda v: v['type'] == 'Teaser', movie.videos()['results']))
-        keywords = movie.keywords()['keywords']
-        from pprint import pprint
-        pprint(movie.reviews()['results'])
-        frontend = {
-            "info": movie.info(),
-            "year": movie.info()['release_date'][:4],
-            "cast": movie.credits()['cast'][:15],
-            "crew": movie.credits()['crew'][:15],
-            "trailers": trailers,
-            "teasers": teasers,
-            "keywords": keywords,
-            "reviews": movie.reviews()['results'],
-            "alt": movie.alternative_titles()['titles']
-        }
+    """This is a function-based view to serve 
+    the movie details for a particular list click,
+    using tmdbsimple API calls.
+
+    Params:
+        request: request from the front-end API call
+        id (int): id of the movie clicked on
+
+    Returns:
+        render method at the details.html endpoint with
+        the required movie details.
+    """
+    movie = tmdb.Movies(id)
+    trailers = list(filter(lambda v: v['type'] == 'Trailer', movie.videos()['results']))
+    teasers = list(filter(lambda v: v['type'] == 'Teaser', movie.videos()['results']))
+    keywords = movie.keywords()['keywords']
+    from pprint import pprint
+    pprint(movie.reviews()['results'])
+    frontend = {
+        "info": movie.info(),
+        "year": movie.info()['release_date'][:4],
+        "cast": movie.credits()['cast'][:15],
+        "crew": movie.credits()['crew'][:15],
+        "trailers": trailers,
+        "teasers": teasers,
+        "keywords": keywords,
+        "reviews": movie.reviews()['results'],
+        "alt": movie.alternative_titles()['titles']
+    }
     return render(request, "details.html", frontend)
